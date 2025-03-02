598ae3deeeb06abad4dbc5bc50d532cbddc4a38f
jules201 <julvoss@uos.de>
2025-03-02 14:46:33 +0100

    added buildfolder

---
+++ chat_client/build/precache-manifest.13e33057355d1ccd8716d36b21b66d66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743e7075adf25518cc74ef05ccc77381",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ada2e578b17664eaf58b",
+    "url": "/static/css/main.1ecf4ad8.chunk.css"
+  },
+  {
+    "revision": "d960c3737275bfcc49c3",
+    "url": "/static/js/2.ea124d61.chunk.js"
+  },
+  {
+    "revision": "1fafe9dc067eebeeb621",
+    "url": "/static/js/3.3a41f75c.chunk.js"
+  },
+  {
+    "revision": "ada2e578b17664eaf58b",
+    "url": "/static/js/main.27c569df.chunk.js"
+  },
+  {
+    "revision": "15fa9da2446a437f9641",
+    "url": "/static/js/runtime~main.f188d0ec.js"
+  }
+]);
