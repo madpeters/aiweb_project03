6c321b5491d9b7139ad8856ba7c91078edd8147f
jules201 <julvoss@uos.de>
2025-03-03 15:41:39 +0100

    build update

---
+++ chat_client/build/precache-manifest.cc9e1c2661e328f1c2c8919669d6a321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0295cc5a1d497e7b1937e133a79fc8cc",
+    "url": "/u064/project3/aiweb_project03/chat_client/build/index.html"
+  },
+  {
+    "revision": "4f6d45d852830adf21ba",
+    "url": "/u064/project3/aiweb_project03/chat_client/build/static/css/main.1ecf4ad8.chunk.css"
+  },
+  {
+    "revision": "d960c3737275bfcc49c3",
+    "url": "/u064/project3/aiweb_project03/chat_client/build/static/js/2.ea124d61.chunk.js"
+  },
+  {
+    "revision": "1fafe9dc067eebeeb621",
+    "url": "/u064/project3/aiweb_project03/chat_client/build/static/js/3.3a41f75c.chunk.js"
+  },
+  {
+    "revision": "4f6d45d852830adf21ba",
+    "url": "/u064/project3/aiweb_project03/chat_client/build/static/js/main.4fe9018b.chunk.js"
+  },
+  {
+    "revision": "44891591dec32c29753d",
+    "url": "/u064/project3/aiweb_project03/chat_client/build/static/js/runtime~main.1fb25801.js"
+  }
+]);
